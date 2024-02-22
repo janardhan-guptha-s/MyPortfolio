e4407eb1a77642b13c13beb2e103fa14a43aed11
janardhangupthasuggala <janardhangupthasuggala@gmail.com>
2024-02-21 23:15:51 -0700

    updates

--- Portfolio/Portfolio-Website-main/assets/js/script.js
+++ Portfolio/Portfolio-Website-main/assets/js/script.js
@@ -133,8 +133,37 @@ function showProjects(projects) {
     });
     // <!-- tilt js effect ends -->
 
+    document.onkeydown = function (e) {
+        if (e.keyCode == 123) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+            return false;
+        }
+    }
   
-
+    //<!--Start of Tawk.to Script-->
+   
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/65d6e5418d261e1b5f63891c/1hn7mv2hp';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+   
+    //<!--End of Tawk.to Script-->
 
 
     /* ===== SCROLL REVEAL ANIMATION ===== */
